a71191f55d670035c2c89fb14056ae836e1ffff1
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-06-07 20:47:52 -0600

    add TableCoin| CoinRow|install Bootstrap|Search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import TableCoins from './components/TableCoins';
 
 function App() {
+
+  const [coins, setCoins]=useState([])
+
+  const [search, setSearch] = useState('')
 
   const getData = async () =>{
     const res = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1");
     console.log(res.data);
+    setCoins(res.data);
   }
 
   useEffect(()=>{
@@ -15,8 +21,11 @@ function App() {
 
 
   return (
-    <div className="App">
-     
+    <div className="container">
+      <div className='row'>
+        <input type="text" placeholder='Search a Coin' className='form-control bg-dark text-light border-0 mt-4 text-center' onChange={e => setSearch(e.target.value) } />
+      <TableCoins coins={coins} search={search} />
+      </div>
     </div>
   );
 }
